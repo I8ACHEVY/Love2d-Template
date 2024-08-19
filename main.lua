9cac64acc9cec7fd655dbee3ba84d60cf7ded02a
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-08-19 18:54:12 -0400

    graphics 2 player movement

--- main.lua
+++ main.lua
@@ -13,3 +13,51 @@ function love.errorhandler(msg)
 end
 
 print("Hello World")
+
+function love.load()
+    P1x = 100;
+    P1y = 100;
+    P2x = 100;
+    P2y = 100;
+    moveSpeed = 100;
+    move = true;
+end;
+
+function love.draw()
+    love.graphics.setColor(0, 200, 300);
+    -- player 1
+    love.graphics.circle("line", P1x, P1y, 50, 50);
+
+    -- player 2
+    love.graphics.rectangle("line", P2x, P2y, 200, 150);
+end;
+
+function love.update(dt)
+    -- Moving Player 1
+    if love.keyboard.isDown("right") then
+        P1x = P1x + moveSpeed * dt;
+    elseif love.keyboard.isDown("left") then
+        P1x = P1x - moveSpeed * dt;
+    end;
+
+    if love.keyboard.isDown("up") then
+        P1y = P1y - moveSpeed * dt;
+    elseif love.keyboard.isDown("down") then
+        P1y = P1y + moveSpeed * dt;
+    end;
+
+    -- Moving Player 2
+    if love.keyboard.isDown("d") then
+        P2x = P2x + moveSpeed * dt;
+    elseif love.keyboard.isDown("a") then
+        P2x = P2x - moveSpeed * dt;
+    end;
+
+    if love.keyboard.isDown("w") then
+        P2y = P2y - moveSpeed * dt;
+    elseif love.keyboard.isDown("s") then
+        P2y = P2y + moveSpeed * dt;
+    else
+        print("Invalid arrow key!");
+    end;
+end;
