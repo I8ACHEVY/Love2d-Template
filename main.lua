f0583c52e75b4923e7e1dcc66be63497aa3cf66a
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-08-19 20:41:55 -0400

    Debug imported and table examples added to main

--- main.lua
+++ main.lua
@@ -1,69 +1,42 @@
-if arg[2] == "debug" then
-    require("lldebugger").start()
-end
-
-local love_errorhandler = love.errorhandler
-
-function love.errorhandler(msg)
-    if lldebugger then
-        error(msg, 2)
-    else
-        return love_errorhandler(msg)
-    end
-end
+--require("example") imports class
+require("Debug")
 
 print("Hello World");
 
 function love.load()
-    P1x = 100;
-    P1y = 100;
-    P2x = 100;
-    P2y = 100;
-    moveSpeed = 100;
-    move = true;
-end;
+    listOfRectangles = {}
+end
 
-function love.draw()
-    love.graphics.setColor(0, 200, 300);
-    -- player 1
-    love.graphics.circle("line", P1x, P1y, 50, 50);
+function createRect()
+    rect = {}
+    --rect["width"] = 100
+    rect.width = 70
+    rect.height = 90
+    rect.x = 100
+    rect.y = 100
+    rect.speed = 100
 
-    -- player 2
-    love.graphics.rectangle("line", P2x, P2y, 200, 150);
-end;
+    table.insert(listOfRectangles, rect)
+end
+
+-- function rect.printHeight()
+--     print(rect.printHeight)
+-- end
+
+function love.keypressed(key)
+    if key == "space" then
+        createRect()
+    end
+end
 
 function love.update(dt)
-    -- Moving Player 1
-    if love.keyboard.isDown("right") then
-        P1x = P1x + moveSpeed * dt;
-    elseif love.keyboard.isDown("left") then
-        P1x = P1x - moveSpeed * dt;
-    else
-        print("Invalid arrow key!");
-    end;
+    for i, v in ipairs(listOfRectangles) do
+        v.x = v.x + v.speed * dt
+    end
+end
 
-    if love.keyboard.isDown("up") then
-        P1y = P1y - moveSpeed * dt;
-    elseif love.keyboard.isDown("down") then
-        P1y = P1y + moveSpeed * dt;
-    else
-        print("Invalid arrow key!");
-    end;
-
-    -- Moving Player 2
-    if love.keyboard.isDown("d") then
-        P2x = P2x + moveSpeed * dt;
-    elseif love.keyboard.isDown("a") then
-        P2x = P2x - moveSpeed * dt;
-    else
-        print("Invalid arrow key!");
-    end;
-
-    if love.keyboard.isDown("w") then
-        P2y = P2y - moveSpeed * dt;
-    elseif love.keyboard.isDown("s") then
-        P2y = P2y + moveSpeed * dt;
-    else
-        print("Invalid arrow key!");
-    end;
-end;
+function love.draw()
+    for i, v in ipairs(listOfRectangles) do
+        love.graphics.rectangle("line", v.x, v.y, v.width, v.height)
+    end
+end
