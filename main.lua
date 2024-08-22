19cfac0f4eccdf2ef28f0ac8851f910fc4404853
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-08-22 18:56:53 -0400

    fresh template

--- main.lua
+++ main.lua
@@ -1,42 +1,12 @@
 --require("example") imports class
 require("Debug")
 
-print("Hello World");
-
 function love.load()
-    listOfRectangles = {}
+
 end
 
-function createRect()
-    rect = {}
-    --rect["width"] = 100
-    rect.width = 70
-    rect.height = 90
-    rect.x = 100
-    rect.y = 100
-    rect.speed = 100
+function love.update()
 
-    table.insert(listOfRectangles, rect)
-end
-
--- function rect.printHeight()
---     print(rect.printHeight)
--- end
-
-function love.keypressed(key)
-    if key == "space" then
-        createRect()
-    end
-end
-
-function love.update(dt)
-    for i, v in ipairs(listOfRectangles) do
-        v.x = v.x + v.speed * dt
-    end
 end
 
 function love.draw()
-    for i, v in ipairs(listOfRectangles) do
-        love.graphics.rectangle("line", v.x, v.y, v.width, v.height)
-    end
-end
